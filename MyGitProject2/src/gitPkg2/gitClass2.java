70167676cf2e1020fdb402cc2d8018dde79a652a
shobanas <shobana.sundara@gmail.com>
2015-08-04 17:28:28 -0700

    save my git projet 2

---
+++ MyGitProject2/src/gitPkg2/gitClass2.java
@@ -0,0 +1,13 @@
+package gitPkg2;
+
+public class gitClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 System.out.println ("in Git Class2");
+		 
+		
+	}
+
+}
